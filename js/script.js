f41786cbee024c2645d4d5e61514e730fcb4ef11
Dmitry Korochkin <abramslost@gmail.com>
2023-02-12 14:20:00 +0300

    add cart

--- js/script.js
+++ js/script.js
@@ -1,19 +1,18 @@
 const products = document.querySelector(".products");
 
-const dataAcquisition = async() => {
-    const response = await fetch("https://fakestoreapi.com/products");
-    const jsonData = await response.json();
-    console.log(jsonData);
+const dataAcquisition = async () => {
+  const response = await fetch("https://fakestoreapi.com/products");
+  const jsonData = await response.json();
+  console.log(jsonData);
 
-    jsonData.forEach(product => {
-      products.innerHTML += `
+  jsonData.forEach(product => {
+    products.innerHTML += `
         <div class="product">
           <h2 class="product__title">${product.title}</h2>
           <img src="${product.image}" alt="" class="product__img">
           <p class="product__description">${product.description}</p>
       </div>
       `;
-    });
-
+  });
 };
 dataAcquisition();
