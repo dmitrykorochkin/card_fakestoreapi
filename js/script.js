819239bf1a513ddac9d521f578da883dc015301c
Dmitry Korochkin <abramslost@gmail.com>
2023-02-12 12:13:16 +0300

    add function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const product = document.querySelector("#product");
-console.log(product);
 
-fetch("https://fakestoreapi.com/products/1")
-  .then(res => res.json())
-  .then(json => console.log(json));
+const dataAcquisition = () => {
+  fetch("https://fakestoreapi.com/products/1")
+    .then(res => res.json())
+    .then(json => console.log(json));
+};
+dataAcquisition();
